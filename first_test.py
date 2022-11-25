53d27b2596fb4f25f66dd744f9342d833282bb36
cyberindygos <cyberindygos@gmail.com>
2022-11-25 21:34:03 +0100

    Initial commit

---
+++ first_test.py
@@ -0,0 +1,6 @@
+
+def func(x):
+    return x + 5
+
+def test_method():
+    assert func(9) == 14
---
+++ multiple_tests.py
@@ -0,0 +1,12 @@
+class Test:
+    def test_method1(self):
+        x = 5
+        y = 0
+        assert x == y
+
+
+    def test_method2(self):
+        a = 14
+        b = 27
+        assert a + 13 == b
+
